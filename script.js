6c9ba514e562a674601909affd5d560592e8e212
Jean Figeuroa <jbster559@gmail.com>
2024-11-27 19:22:34 -0800

    Added console logs

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("I said this before in the CSS assignment but the in-class lectures were really helpful for my learning")
+console.log("With the tight 8 week schdule im not sure if adding anything is feasible but a going futher into arrays and list would be nice")
+console.log("Previously an error of any sort would cause a crash in my python/godot projects. I was expecting to learn bug handling in this course which I did")
+console.log("Due to my small but still present expiernce in Godot and Python I was familiar with some concepts such as event listeners and arrays but I did learn about dealing with errors (in a way that doesnt cuse crashes)")
